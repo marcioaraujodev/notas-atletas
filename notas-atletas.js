120605ce7ceccc05ae8deae6bf9972d1d15c697f
Márcio Araújo <marcioaraujoufrn@gmail.com>
2026-05-17 10:46:28 -0300

    "Projeto de certificação 1 - Jornada DEV Senai - Pontuação dos atletas"

---
+++ notas-atletas.js
@@ -0,0 +1,46 @@
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas: [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+
+function calculaMedia(atletas) {
+
+    for (let i = 0; i < atletas.length; i++) {
+
+        let notasOrdenadas = atletas[i].notas.sort(function(a, b) {
+            return a - b;
+        });
+
+        let notasValidas = notasOrdenadas.slice(1, 4);
+
+        let soma = 0;
+
+        notasValidas.forEach(function(nota) {
+            soma += nota;
+        });
+
+        let media = soma / notasValidas.length;
+
+        console.log(`Atleta: ${atletas[i].nome}`);
+        console.log(`Notas Obtidas: ${notasOrdenadas}`);
+        console.log(`Média Válida: ${media}`);
+        console.log("");
+    }
+
+}
+
+calculaMedia(atletas);
